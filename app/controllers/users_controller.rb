f221ee931e0528d3bc0a86962f6f1ee73b7635bd
kazu-taka <nike.taka@gmail.com>
2016-02-07 01:35:00 +0900

    第２課題ー問G実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,12 @@
 class UsersController < ApplicationController
-  def index
+  before_action :set_user, only: [:show]
+
+  def show
+    @tweets = Tweet.where(user_id: @user.id).page(params[:page])
+  end
+
+  private
+  def set_user
+    @user = User.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :tweets
   devise_for :users
-  get 'users/index'
+  get 'users/id' => 'users#show', as: :user
   root 'tweets#index'
 
   # devise_for :users
