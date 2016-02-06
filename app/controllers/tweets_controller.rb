3af53e9e8087e86351afdc446e252dc3d4e2a01d
kazu-taka <nike.taka@gmail.com>
2016-02-06 15:58:22 +0900

    第２課題ー問D実装

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,10 +1,11 @@
 class TweetsController < ApplicationController
-  before_action :authenticate_user!, only: [:new]
+  before_action :authenticate_user!, only: [:create]
   before_action :set_tweet, only: [:show]
 
   def index
     @tweets = Tweet.page(params[:page])
     @tweet = Tweet.new
+
   end
 
   def create
@@ -13,8 +14,9 @@ class TweetsController < ApplicationController
     respond_to do |format|
       if @tweet.save
         format.html { redirect_to @tweet, notice: 'Restaurant was successfully created.' }
-        format.json { render :index, status: :created, location: @tweet }
+        format.json { render :show, status: :created, location: @tweet }
       else
+        @tweets = Tweet.page(params[:page])
         format.html { render :index }
         format.json { render json: @tweet.errors, status: :unprocessable_entity }
       end
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -3,4 +3,8 @@ class Tweet < ActiveRecord::Base
   has_many :favorites
   default_scope -> { order(created_at: :desc) }
   paginates_per 10
+
+  validates :content, length:
+             { in: 2..140,
+               message: "は2文字以上140文字以内で入力してください。" }
 end
